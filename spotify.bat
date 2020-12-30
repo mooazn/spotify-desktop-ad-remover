c1093ec973a19a360b942638e305c76d128941d4
Mooaz Nasir <mooaz09@gmail.com>
2020-12-29 20:55:03 -0800

    batch file

---
+++ spotify.bat
@@ -0,0 +1,5 @@
+START "Location of Spotify, including the .exe (i.e. C:\Users\name\AppData\Roaming\Spotify\Spotify.exe)"  
+
+@echo off
+python C:\Users\name\Downloads\SpotifyThing\main.py %*
+pause
