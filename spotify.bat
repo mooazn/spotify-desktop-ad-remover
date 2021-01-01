a8c9143a5c3fbf2e76450644b615988667a6315b
Mooaz Nasir <mooaz09@gmail.com>
2020-12-31 17:14:50 -0800

    Update spotify.bat

--- spotify.bat
+++ spotify.bat
@@ -1,4 +1,4 @@
-:: The creation of the batch file is optional. If you intend to use the script everyday then this batch file is handy.
+:: The creation of the batch file is optional. If you intend to use the script everyday then this batch file is handy if turned into a shortcut.
 
 START "C:\Users\name\AppData\Roaming\Spotify\Spotify.exe"  
 :: For above: replace and give location of the Spotify.exe itself. 
