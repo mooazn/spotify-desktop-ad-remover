b86e2c85432c22834e69fea71fc160aef7ed9bfd
Mooaz Nasir <mooaz09@gmail.com>
2021-01-06 15:25:58 -0800

    More fixes. Seems to work fine.

--- spotify.py
+++ spotify.py
@@ -18,7 +18,8 @@ import win32com.client
 """
 below is not necessary. you can simply put the location of Spotify inside the loc
 variable if you don't want to run the below commands. if you do run below, it is only
-meant to be run ONCE since result is stored in a file.
+meant to be run ONCE since result is stored in a file. once ran, it can be commented
+again (except for the file reading part which reads the location into a variable).
 
 command = 'dir spotify.exe /s | findstr "Directory of .:[.]*"'
 proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True, cwd='/')
@@ -35,20 +36,19 @@ loc = f.read()
 f.close()
 """
 
-loc = 'Location of Spotify, i.e. -> C:\\Users\\USERNAME\\AppData\\Roaming\\Spotify'  # just use your file location if making .bat file
+loc = 'Location of Spotify, i.e. -> C:\\Users\\USERNAME\\AppData\\Roaming\\Spotify'
 
 installer = ChromeDriverManager().install()
 options = Options()
 options.headless = True
 driver = webdriver.Chrome(installer, options=options)
 driver.get('https://accounts.spotify.com/en/login/')
-driver.find_element_by_xpath('//*[@id="login-username"]').send_keys('email')
-driver.find_element_by_xpath('//*[@id="login-password"]').send_keys('password')
+driver.find_element_by_xpath('//*[@id="login-username"]').send_keys('email')  # Spotify email/username
+driver.find_element_by_xpath('//*[@id="login-password"]').send_keys('password')  # Spotify password
 driver.find_element_by_xpath('//*[@id="login-button"]').click()
-time.sleep(3)
 
 
-def windowEnumerationHandler(hwnd, top):  # see line 109
+def windowEnumerationHandler(hwnd, top):  # see line 110
     top.append((hwnd, win32gui.GetWindowText(hwnd)))
 
 
@@ -76,7 +76,7 @@ if '<token>' in headers['Authorization']:
 new_token_registered = int(time.time())
 song_id = ''
 
-while True:
+while True:  # this can be replaced with a check to see if Spotify is running
     print(headers['Authorization'])
     response = requests.get('https://api.spotify.com/v1/me/player/currently-playing', headers=headers)
     try:
@@ -106,6 +106,7 @@ while True:
             os.kill(int(p_id), signal.SIGTERM)  # nice
         win32process.CreateProcess(None, loc + '\\Spotify.exe', None, None, False, 0, None, None, win32process.STARTUPINFO())
         time.sleep(2)
+        # ------
         # for below (and line 51): https://stackoverflow.com/questions/54918333/how-to-maximize-an-inactive-window - cosminm's post
         top_windows = []
         win32gui.EnumWindows(windowEnumerationHandler, top_windows)
@@ -118,11 +119,15 @@ while True:
                 win32gui.SetForegroundWindow(i[0])
                 win32gui.ShowWindow(i[0], win32con.SW_MAXIMIZE)
                 break
+        # ------
         pyautogui.sleep(1)
         pyautogui.press('space')
         pyautogui.sleep(1)
+        if int(time.time() - new_token_registered) >= 3500:  # in the extremely rare case that the token expires while checking for repeat
+            headers['Authorization'] = 'Bearer ' + generate_new_token()
+            new_token_registered = int(time.time())
         new_response = requests.get('https://api.spotify.com/v1/me/player/currently-playing', headers=headers)
-        is_new_thing_track = True if json.loads(response.text)['currently_playing_type'] == 'track' else False
+        is_new_thing_track = True if json.loads(new_response.text)['currently_playing_type'] == 'track' else False
         if is_new_thing_track:
             new_song_id = json.loads(new_response.text)['item']['id']
             if str(song_id) == str(new_song_id):  # if the song is the same as the one that was playing before the ad, skip it (ctrl + right) is the shortcut
@@ -137,11 +142,11 @@ while True:
         pyautogui.getActiveWindow().minimize()
     else:
         print('Playing track...')
-    if int(time.time()) - new_token_registered >= 3600:
+    if int(time.time()) - new_token_registered >= 3500:  # changed to a little less than an hour just to be safe
         headers['Authorization'] = 'Bearer ' + generate_new_token()
         new_token_registered = int(time.time())
         continue
-    time.sleep(10)  # this can be lowered. didn't try because fear of being rate limited.
+    time.sleep(5)
 
 
 @atexit.register
